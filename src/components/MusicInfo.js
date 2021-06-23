6643364faf3e6f7be490a132757121fdeb24e492
Hwanhee Chae <hh@hwan.me>
2021-06-23 12:02:40 +0900

    add feature: add side coloring which displays introduced version (such as vivid wave, exceed gear ...)

--- src/components/MusicInfo.js
+++ src/components/MusicInfo.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Typography } from '@material-ui/core';
 
 export default function MusicInfo(props) {
-  const { title_name, artist_name, distribution_date, bpm_min, bpm_max } = props.info;
+  const { title_name, artist_name, distribution_date, bpm_min, bpm_max, version } = props.info;
 
   const datestr = (date) => {
     return `${date.substr(0, 4)}/${date.substr(4, 2)}/${date.substr(6, 2)}`;
@@ -13,8 +13,17 @@ export default function MusicInfo(props) {
     return min !== max ? `${min/100} ~ ${max/100}` : `${min/100}`;
   }
 
+  const introduced = {
+    1: 'booth',
+    2: 'infiniteInfection',
+    3: 'gravityWars',
+    4: 'heavenlyHaven',
+    5: 'vividWave',
+    6: 'exceedGear',
+  }
+
   return (
-    <div>
+    <div className={`musicInfo ${introduced[version]}`}>
       <Typography>{title_name}</Typography>
       <Typography>{artist_name}</Typography>
       <Typography>{datestr(distribution_date)}</Typography>
