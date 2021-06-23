1dcd6e3a31abbb73eddb890950454134c64f5410
Hwanhee Chae <hh@hwan.me>
2021-06-23 10:36:39 +0900

    add feature: add loading state and Loader to Search component, add styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,10 +6,13 @@ import { TextField } from '@material-ui/core';
 import axios from 'axios';
 
 import MusicList from './MusicList';
+import Loader from './Loader';
 
 export default function Search() {
   const [title, setTitle] = useState('');
   const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const handleInput = (event) => {
     if (title !== event.target.value.trim()) {
       setTitle(event.target.value.trim());
@@ -20,11 +23,13 @@ export default function Search() {
     // wait 3 seconds after user input, prevent requesting by each character
     const timeout = setTimeout(() => {
       if (title.length) {
+        setLoading(true);
         axios.get('https://api.sdvx.org/v1', {
           params: { title }
         }).then((response) => {
           if (response.config.params.title === title) {
             setResult(response.data);
+            setLoading(false);
           }
         });
       }
@@ -36,7 +41,13 @@ export default function Search() {
     <div id='container'>
       <div id='innerContainer'>
         <TextField label='Title' style={{ width: '100%' }} onInput={handleInput} />
-        <MusicList music={result} />
+        <div id='content'>
+          { loading ? (
+            <Loader />
+          ) : (
+            <MusicList music={result} />
+          )}
+        </div>
       </div>
     </div>
   )
