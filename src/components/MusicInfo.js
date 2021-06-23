aa71dc56e8a0d924f861857a77dfb3f14edaed29
Hwanhee Chae <hh@hwan.me>
2021-06-23 11:48:30 +0900

    add feature: display bpm by min ~ max or just a number if min == max

--- src/components/MusicInfo.js
+++ src/components/MusicInfo.js
@@ -3,10 +3,14 @@ import React from 'react'
 import { Typography } from '@material-ui/core';
 
 export default function MusicInfo(props) {
-  const { title_name, artist_name, distribution_date } = props.info;
+  const { title_name, artist_name, distribution_date, bpm_min, bpm_max } = props.info;
 
   const datestr = (date) => {
     return `${date.substr(0, 4)}/${date.substr(4, 2)}/${date.substr(6, 2)}`;
+  }
+
+  const bpmstr = (min, max) => {
+    return min !== max ? `${min/100} ~ ${max/100}` : `${min/100}`;
   }
 
   return (
@@ -14,6 +18,7 @@ export default function MusicInfo(props) {
       <Typography>{title_name}</Typography>
       <Typography>{artist_name}</Typography>
       <Typography>{datestr(distribution_date)}</Typography>
+      <Typography>BPM {bpmstr(bpm_min, bpm_max)}</Typography>
     </div>
   )
 }
