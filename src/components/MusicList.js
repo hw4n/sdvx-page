175e220898d06613297e9eed30822b1e0b249abf
Hwanhee Chae <hh@hwan.me>
2021-11-03 09:20:55 +0900

    fix: use normal grid size

--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -18,7 +18,7 @@ export default function MusicList(props) {
     <Grid container xs={12} spacing={2} justifyContent='center'>
       {music.map(m => {
         return (
-          <Grid item xs={11} md={5} lg={4}>
+          <Grid item xs={12} md={6} lg={4}>
             <Card sx={{ display: 'flex' }}>
               <CardMedia
                 component="img"
